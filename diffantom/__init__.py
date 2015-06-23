a3aadbcd5448e88f7fa435d351f22359b321ee8c
Oscar Esteban <code@oscaresteban.es>
2015-06-23 12:46:50 +0200

    update paper

--- diffantom/__init__.py
+++ diffantom/__init__.py
@@ -3,4 +3,6 @@
 # @Author: oesteban
 # @Date:   2015-06-15 14:50:01
 # @Last Modified by:   oesteban
-# @Last Modified time: 2015-06-15 14:50:01
+# @Last Modified time: 2015-06-23 12:31:14
+
+from interfaces import PhantomasSticksSim
