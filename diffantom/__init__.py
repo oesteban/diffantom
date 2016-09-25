5b41bfc14dab4f25fe199d44bbd9e9dc7e92c780
oesteban <code@oscaresteban.es>
2016-09-25 10:51:30 -0700

    add find_packages exceptions, fix main __init__

--- diffantom/__init__.py
+++ diffantom/__init__.py
@@ -5,5 +5,18 @@
 # @Last Modified by:   Oscar Esteban
 # @Last Modified time: 2015-06-23 13:05:13
 from __future__ import absolute_import
+from .info import (
+    __version__,
+    __author__,
+    __email__,
+    __maintainer__,
+    __copyright__,
+    __credits__,
+    __license__,
+    __status__,
+    __description__,
+    __longdesc__
+)
+
 from .interfaces import PhantomasSticksSim, LoadSamplingScheme
 from .workflows import gen_diffantom, gen_model, finf_bundles
